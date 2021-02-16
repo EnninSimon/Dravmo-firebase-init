dad67f5c8cee0d02bcd163974707c64a6449c018
Ennin Simon <68439780+EnninSimon@users.noreply.github.com>
2021-02-16 02:47:50 +0000

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -11,7 +11,7 @@ function AlertDismissible() {
           <Alert.Heading>Welcome to Dravmo</Alert.Heading>
           <p>
             Dravmo is a project-based web-app to help constructors or real-estate owners to post their new, ongoing, and completed projects online.
-            You cannot upload your image in the form at the moment(<b>It will cause erros when editing the form</b>). Our team is currently working on that issue. We hope to solve it soon. Thank you!
+            You cannot upload your image in the form at the moment. Our team is currently working on that issue. We hope to solve it soon. Thank you!
           </p>
           <hr />
           <div className="d-flex justify-content-end">
